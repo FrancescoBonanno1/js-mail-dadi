97b5e4f8e093a4bf5ca75908fd922c1f1f292b32
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-11-01 18:53:27 +0100

    esercizio non completato

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-const mail = document.getElementById("fill").innerHTML;
-let authorized = ["francescosoapbonanno@gmail.com" , "stalker@gmail.com" , "pisnelopisnelo@gmail.com" , "CIA@yahoo.com" , "vladimirputin@hotmail.ru"]
+const mail = document.getElementById("fill");
+let authorized = ["francescosoapbonanno@gmail.com" , "stalker@gmail.com" , "pisnelopisnelo@gmail.com" , "CIA@yahoo.com" , "vladimirputin@hotmail.ru"];
+let button = document.getElementById("proceed");
 
 if( mail == authorized )
 {
-    document.getElementById("proceed").addEventListener("click", function() {
+    button.addEventListener("click", function() {
         alert("La stavamo aspettando, maestro");
     })}
 
 else {
-    document.getElementById("proceed").addEventListener("click", function (){
+    button.addEventListener("click", function (){
         Error("E tu chi diavolo sei? Fuori di qui!");
     })
 }
