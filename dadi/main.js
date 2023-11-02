5ce40307815c5a6930e85c5738b0370a0d2f292e
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-11-02 12:45:20 +0100

    feat: aggiunge "pareggio"

--- dadi/main.js
+++ dadi/main.js
@@ -2,8 +2,14 @@ const player = Math.floor ((Math.random() *6) +1);
 const cpu =  Math.floor ((Math.random() *6) +1);
 
 if(player > cpu){
-    document.getElementById("vinto").innerHTML= (`Complimenti, hai ottenuto ${player}, mentre il PC ha ottenuto ${cpu}, hai vinto!`)
+    document.getElementById("risultato").innerHTML=`Complimenti, hai ottenuto ${player}, mentre il PC ha ottenuto ${cpu}, hai vinto!`
+    document.getElementById("risultato").classList.add("vinto")
 }
-else{
-    document.getElementById("perso").innerHTML= (`Mi dispiace amico, il PC ha ottenuto ${cpu}, mentre tu hai ottenuto ${player}, hai perso.`)
+else if(player < cpu){
+    document.getElementById("risultato").innerHTML=`Mi dispiace amico, il PC ha ottenuto ${cpu}, mentre tu hai ottenuto ${player}, hai perso.`
+    document.getElementById("risultato").classList.add("perso")
+}
+else {
+    document.getElementById("risultato").innerHTML=`Ti è endata bene amico, hai ottenuto ${player}, ed il computer ha ottenuto ${cpu}, avete pareggiato`
+    document.getElementById("risultato").classList.add("pareggio")
 }
